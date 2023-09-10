8956df24e070b262dc52999ed9446f53106de736
Roman Kutsenko <Kutsenkor.n@gmail.com>
2023-09-10 19:41:57 +0300

    rel.v1

---
+++ H_W_Mod4_1/ReqresApiHw/Models/ReqresUserRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_W_Mod4_1.ReqresApiHw
+{
+    internal sealed class ReqresUserRequest
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        public string Avatar { get; set; }
+    }
+}
